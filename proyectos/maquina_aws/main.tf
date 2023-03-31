dc9523f5e13520de85643036872e64ed08731d98
Ubuntu <ubuntu@ip-172-31-14-8.eu-west-1.compute.internal>
2023-03-31 10:55:36 +0000

    version2

--- proyectos/maquina_aws/main.tf
+++ proyectos/maquina_aws/main.tf
@@ -23,8 +23,9 @@ resource "aws_instance" "mi_servidor" {
                     #  NS/NC = I
                     #  NC = Ivan (pero si sabe)
   #depends_on = [aws_key_pair.mis_claves] # ESTO ES UNA MIERDA Y MUY MALA PRACTICA PDE HECHO !!!
-  key_name      = aws_key_pair.mis_claves.key_name  # GUAY !!!!!
-
+  key_name          = aws_key_pair.mis_claves.key_name  # GUAY !!!!!
+  security_groups   = [aws_security_group.mi_security_group.name]
+  
   tags = {
         Name = "maquina-${var.nombre_despliegue}"
   }
@@ -58,4 +59,59 @@ module "claves_ssh" {
 resource "aws_key_pair" "mis_claves" {
   key_name   = "claves-prueba-${var.nombre_despliegue}"
   public_key = module.claves_ssh.publicKey.openssh
+}
+
+resource "aws_security_group" "mi_security_group" {
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+        Name = "security-group-${var.nombre_despliegue}"
+  }
+}
+
+module "pruebas" {
+    source          = "../test"
+    host            = "${aws_instance.mi_servidor.public_ip}"
+    ping            = {}
+    ssh_connection  = {
+                            user       = "ubuntu"
+                            privateKey = module.claves_ssh.privateKey.pem
+                        }
+}
+
+output "resultado_ping"{
+    value = module.pruebas.ping_test
 }
